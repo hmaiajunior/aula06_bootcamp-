e5933465add1cea341097736643193f49e0f8345
hmaiajunior <juniorce04@gmail.com>
2024-12-01 22:55:44 -0300

    adicionando arquivo main

---
+++ main.py
@@ -0,0 +1,6 @@
+# flake8 --> lib que dá as dicas de correções no seu código de acordo com o pep8
+# isort
+# blake
+# bandit
+
+# pre-commit --> avalia o seu código para garantir uma série de regras antes de vc realizar o seu commit
